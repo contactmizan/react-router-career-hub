09bf2a83aca8b341df0f6d09aea5521e4905e81f
contactmizan <contactmizan1206@gmail.com>
2024-05-02 23:54:01 +0600

    added job featured

--- src/component/FeaturedJobs/FeaturedJobs.jsx
+++ src/component/FeaturedJobs/FeaturedJobs.jsx
@@ -4,6 +4,8 @@ import Job from "../Job/Job";
 
 const FeaturedJobs = () => {
     const [jobs, setJobs] = useState([]);
+    //this is not the best way to load show all data
+    const [dataLength, setDataLength] = useState(4);
 
     useEffect(() => {
         fetch('jobs.json')
@@ -16,10 +18,13 @@ const FeaturedJobs = () => {
                 <h2 className="text-5xl">Featured Jobs: {jobs.length}</h2>
                 <p>Explore thousands of job opportunities with all the information you need. Its your future</p>
             </div>
-            <div>
+            <div className="grid grid-cols-2">
                 {
-                    jobs.map(job => <Job key={job.id} job={job}></Job>)
+                    jobs.slice(0, dataLength).map(job => <Job key={job.id} job={job}></Job>)
                 }
+            </div>
+            <div className={dataLength === jobs.length && 'hidden'}>
+                <button onClick={() => setDataLength(jobs.length)} className="btn btn-primary">Show All Jobs</button>
             </div>
         </div>
     );
--- src/component/Job/Job.jsx
+++ src/component/Job/Job.jsx
@@ -1,11 +1,26 @@
-
-
+import { IoLocationOutline } from "react-icons/io5";
+import { HiOutlineCurrencyDollar } from "react-icons/hi2";
 const Job = ({ job }) => {
-    const { logo } = job;
+    const { logo, job_title, company_name, remote_or_onsite, location, job_type, salary } = job;
 
     return (
-        <div>
-            <img src={logo} alt="" />
+        <div className="card card-compact bg-base-100 shadow-xl mb-4">
+            <figure><img src={logo} alt="Shoes" /></figure>
+            <div className="card-body">
+                <h2 className="card-title">{job_title}</h2>
+                <p>{company_name}</p>
+                <div>
+                    <button className="px-5 py-2 font-extrabold border rounded border-[#7E90FE] text-[#7E90FE] mr-4">{remote_or_onsite}</button>
+                    <button className="px-5 py-2 font-extrabold border rounded border-[#7E90FE] text-[#7E90FE] mr-4">{job_type}</button>
+                </div>
+                <div className="mt-4 flex">
+                    <h2 className="flex mr-4"><IoLocationOutline className="text-2xl mr-2"></IoLocationOutline>{location}</h2>
+                    <h2 className="flex "><HiOutlineCurrencyDollar className="text-2xl mr-2"></HiOutlineCurrencyDollar>{salary}</h2>
+                </div>
+                <div className="card-actions ">
+                    <button className="btn btn-primary">View Details</button>
+                </div>
+            </div>
         </div>
     );
 }
