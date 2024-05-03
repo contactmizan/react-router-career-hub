1bb58323b786202a6c49fbdc44d115a9e63afa64
contactmizan <contactmizan1206@gmail.com>
2024-05-03 23:35:34 +0600

    set applied job & display jobsfilter

--- src/component/JobDetails/JobDetails.jsx
+++ src/component/JobDetails/JobDetails.jsx
@@ -1,5 +1,8 @@
 import { useLoaderData, useParams } from "react-router-dom";
+import { ToastContainer, toast } from 'react-toastify';
 
+import 'react-toastify/dist/ReactToastify.css';
+import { saveJobApplication } from "../../utility/localStorage";
 
 const JobDetails = () => {
     const jobs = useLoaderData();
@@ -7,9 +10,14 @@ const JobDetails = () => {
     const idInt = parseInt(id);
     const job = jobs.find(job => job.id === idInt);
     console.log(job);
+
+    const handleApplyJob = () => {
+        saveJobApplication(idInt);
+        toast('You have successfully applied');
+    }
     return (
         <div>
-           
+
             <div className="grid gap-4 md:grid-cols-4">
                 <div className="border md:col-span-3">
                     <h2 className="text-4xl">Details coming here</h2>
@@ -18,9 +26,10 @@ const JobDetails = () => {
                 </div>
                 <div className="border">
                     <h2 className="text-2xl">Side things</h2>
-                    <button className="btn btn-primary w-full">Apply Now</button>
+                    <button onClick={handleApplyJob} className="btn btn-primary w-full">Apply Now</button>
                 </div>
             </div>
+            <ToastContainer />
         </div>
     );
 }
